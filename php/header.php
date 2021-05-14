360a1c169bc5a73078f338b4da7e2c6adefbff70
Bandile_M <bandilemashile7@gmail.com>
2021-05-14 08:45:30 +0200

    Add files via upload

---
+++ php/header.php
@@ -0,0 +1,76 @@
+<style>
+header{
+ 
+  
+  padding: 2px;
+  background:bg-dark;
+  color: black;
+}
+</style>
+<header id="header" >
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
+        <a href="index.php"  class="navbar-brand">
+            <h3 class="px-5"  >
+               Fear of Being Innocent
+            </h3>
+        </a>
+        <button class="navbar-toggler"
+            type="button"
+                data-toggle="collapse"
+                data-target = "#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon" ></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="mr-auto" ></div>
+            <div class="navbar-nav" >
+			
+			 <a href="index.php" class="nav-item nav-link active">
+                    <h5 class="px-5 cart">
+                      Home
+                    </h5>
+                </a>
+			<a href="about.php" class="nav-item nav-link active">
+                    <h5 class="px-5 cart">
+                      About
+                    </h5>
+                </a>
+				
+				<a href="blog.php" class="nav-item nav-link active">
+                    <h5 class="px-5 cart">
+                      Gallery
+                    </h5>
+                </a>
+				
+				
+			
+                <a href="cart.php"  class="nav-item nav-link active">
+                    <h5 class="px-5 cart">
+                        <i class="fas fa-shopping-cart"></i> Cart
+                        <?php
+
+                        if (isset($_SESSION['cart'])){
+                            $count = count($_SESSION['cart']);
+                            echo "<span id=\"cart_count\" class=\"text-warning bg-light\">$count</span>";
+                        }else{
+                            echo "<span id=\"cart_count\" class=\"text-warning bg-light\">0</span>";
+                        }
+
+                        ?>
+                    </h5>
+                </a>
+				
+            </div>
+        </div>
+
+    </nav>
+</header>
+
+
+
+
+
+
